df4604ad10e9bd0203758786ad6b9e1b1e016578
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-17 21:53:22 -0500

    implemented use of vec3 class in ppm image generator

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "vec3.h"
 
 int main() {
     int nx = 200;
@@ -8,13 +9,11 @@ int main() {
 
     for (int y = ny-1; y >= 0; y--){
         for (int x = 0; x < nx; x++){
-            float r = float(x) / float(nx);
-            float g = float(y) / float(ny);
-            float b = 0.2;
+            vec3 col(float(x) / float(nx), float(y) / float(ny), 0.2);
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
 
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
