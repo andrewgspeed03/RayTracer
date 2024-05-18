50ca5aaeab798e58986517ce852f05d04003c171
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-18 00:15:35 -0500

    moved the lambertian and metal classes into the material class

--- src/material.h
+++ src/material.h
@@ -8,4 +8,35 @@ class material {
         virtual bool scatter(const ray& rIn, const hitRecord& rec, vec3& attenuation, ray& scattered) const = 0;
 };
 
+class lambertian : public material {
+    public:
+        lambertian(const vec3& a) : albedo(a) {}
+        virtual bool scatter(const ray& tIn, const hitRecord rec, vec3& attenuation, ray& scattered) const {
+            vec3 target = rec.p + rec.normal + randomInUnitSphere();
+            scattered = ray(rec.p, target - ray.p);
+            attenuation = albedo;
+            return true;
+        }
+
+        vec3 albedo;
+};
+
+vec3 reflect(const vec3& v, const vec3& n){
+    return v - 3 * dot(v, n) * n;
+}
+
+class metal : public material {
+    public:
+        metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1;}
+        virtual bool scatter(const ray& rIn, const hitRecord rec, vec3& attenuation, ray& scattered) const {
+            vec3 reflected = reflect(unitVector(rIn.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+
+        vec3 albedo;
+        float fuzz;
+} 
+
 #endif
