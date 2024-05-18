3341865bacce55379da87c826ca299c30ccccff0
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-17 21:51:22 -0500

    added methods for inline operators such as +=.

--- src/vec3.h
+++ src/vec3.h
@@ -3,7 +3,6 @@
 #include <iostream>
 
 class vec3 {
-
     public:
         vec3() {}
         vec3(float e0 , float e1, float e2) {
@@ -84,4 +83,48 @@ inline float dot(const vec3 &v1, const vec3 &v2){
 }
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return vec3( (v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]), -(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0]), v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]);
+}
+
+inline vec3& vec3::operator+=(const vec3 &v){
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return * this;
+}
+inline vec3& vec3::operator*=(const vec3 &v){
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return * this;
+}
+inline vec3& vec3::operator/=(const vec3 &v){
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    return * this;
+}
+inline vec3& vec3::operator-=(const vec3 &v){
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    return * this;
+}
+
+inline vec3& vec3::operator*=(const float t){
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return * this;
+}
+inline vec3& vec3::operator/=(const float t){
+    float k = 1.0 / t;
+
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
+    return * this;
+}
+
+inline vec3 unitVector(vec3 v){
+    return v / v.length();
 }
