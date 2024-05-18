f966bbf435384dae8cfde2408f5843bfe135bc65
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-17 22:06:34 -0500

    painted the computer screen in reference to the users eye as the origin point.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,11 @@
 #include <iostream>
-#include "vec3.h"
+#include "ray.h"
+
+vec3 color(const ray& r){
+    vec3 unitDirection = unitVector(r.direction());
+    float t = 0.5 * (unitDirection.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main() {
     int nx = 200;
@@ -7,9 +13,18 @@ int main() {
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
+    vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
     for (int y = ny-1; y >= 0; y--){
         for (int x = 0; x < nx; x++){
-            vec3 col(float(x) / float(nx), float(y) / float(ny), 0.2);
+            float u = float(x) / float(nx);
+            float v = float(y) / float(ny);
+
+            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+            vec3 col = color(r);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
