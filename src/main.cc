9fa90e42552e3d29a42f3ce1453c27aa55a4de7c
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-17 23:14:49 -0500

    moved camera into its own class and modified it so that is smooths the edges of the objects

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 #include "sphere.h"
 #include "hittableList.h"
 #include "float.h"
+#include "camera.h"
+
 
 vec3 color(const ray& r, hittable *world){
     hitRecord rec;
@@ -17,27 +20,26 @@ vec3 color(const ray& r, hittable *world){
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-
-    vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
 
     hittable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
     hittable *world = new hittableList(list, 2);
 
+    camera cam;
     for (int y = ny-1; y >= 0; y--){
         for (int x = 0; x < nx; x++){
-            float u = float(x) / float(nx);
-            float v = float(y) / float(ny);
-
-            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
-            vec3 col = color(r, world);
-
+            for (int s = 0; s < ns; s ++){
+                float u = float(x + drand48()) / float(nx);
+                float v = float(y + drand48()) / float(ny);
+                ray r = cam.getRay(u, v);
+                vec3 p = r.pointAtParameter(2.0);
+                col += color(r, world);
+            }
+            col /= float(ns);
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
