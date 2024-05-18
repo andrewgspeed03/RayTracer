9213cf88a258eb5ce162173b2fdb362338cc1bf0
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-17 23:21:30 -0500

    added method to shaded to sphere matte based of if the ray should be absorbed or reflected

--- src/main.cc
+++ src/main.cc
@@ -5,11 +5,20 @@
 #include "float.h"
 #include "camera.h"
 
+vec3 randomInUnitSphere() {
+    vec3 p;
+    do 
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    while (p.squaredLength() >= 1.0);
+    return p;
+}
 
 vec3 color(const ray& r, hittable *world){
     hitRecord rec;
-    if (world -> hit(r, 0.0, MAXFLOAT, rec))
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+    if (world -> hit(r, 0.0, MAXFLOAT, rec)){
+        vec3 target = rec.p + rec.normal + randomInUnitSphere();
+        return 0.5 * color( ray(rec.p, targer - rec.p), world);
+    }
     else{
         vec3 unitDirection = unitVector(r.direction());
         float t = 0.5 * (unitDirection.y() + 1.0);
@@ -32,6 +41,7 @@ int main() {
     camera cam;
     for (int y = ny-1; y >= 0; y--){
         for (int x = 0; x < nx; x++){
+            vec3 col(0, 0, 0)
             for (int s = 0; s < ns; s ++){
                 float u = float(x + drand48()) / float(nx);
                 float v = float(y + drand48()) / float(ny);
