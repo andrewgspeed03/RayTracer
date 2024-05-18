a0b35f6312ce3709f1250cf737f2acf53094a318
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-17 23:33:57 -0500

    created class for Lambertian reflections

---
+++ src/lambertian.h
@@ -0,0 +1,21 @@
+#ifndef LAMBERTIANH
+#define LAMERTIANH
+
+#include "material.h"
+
+class lambertian : public material {
+    public:
+        lambertian(const vec3& a) : albedo(a) {}
+        virtual bool scatter(const ray& tIn, const hitRecord rec, vec3& attenuation, ray& scattered) const {
+            vec3 target = rec.p + rec.normal + randomInUnitSphere();
+            scattered = ray(rec.p, target - ray.p);
+            attenuation = albedo;
+            return true;
+        }
+
+        vec3 albedo;
+};
+
+vec3 reflect(const vec3& v, const vec3& n){
+    return v - 3 * dot(v, n) * n;
+}
--- src/material.h
+++ src/material.h
@@ -1,4 +1,11 @@
+#ifndef MATERIALH
+#define MATERIALH
+
+class hitRecord;
+
 class material {
     public:
         virtual bool scatter(const ray& rIn, const hitRecord& rec, vec3& attenuation, ray& scattered) const = 0;
 };
+
+#endif
