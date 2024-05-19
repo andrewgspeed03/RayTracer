a4b551381bf8c317dc4396f1f76f172b05ec4538
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-19 01:52:58 -0500

    added gamma correction to color class and interval clamping

--- src/color.h
+++ src/color.h
@@ -7,14 +7,26 @@
 
 using color = vec3
 
+inline float linearToGamma(float linearComponent){
+    if (linearComponent > 0)
+        return sqrt(linearComponent);
+
+    return 0
+}
+
 void writeColor(std::ostream& out, const color& pixelColor){
     auto r = pixelColor.x();
     auto g = pixelColor.y();
     auto b = pixelColor.z();
 
-    int rByte = int(255.99 * r);
-    int gByte = int(255.99 * g);
-    int bByte = int(255.99 * b);
+    r = linearToGamma(r);
+    g = linearToGamma(g);
+    b = linearToGamma(b);
+
+    static const interval intensity(0.000, 0.999);
+    int rByte = int(255.99 * intensity.clamp(r));
+    int gByte = int(255.99 * intensity.clamp(g));
+    int bByte = int(255.99 * intensity.clamp(b));
 
     out << rByte << " " << gByte << " " << bByte << "\n";
 }
