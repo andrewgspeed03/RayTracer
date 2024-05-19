6d251e486832834c021d33529bdfa698b6b2f6ad
andrewgspeed03 <aa.goodspeed@gmail.com>
2024-05-19 00:43:26 -0500

    changed hittable list into a shared_ptr of hittable objects from a list

--- src/hittableList.h
+++ src/hittableList.h
@@ -3,30 +3,36 @@
 
 #include "hittable.h"
 
+#include <vector>
+
 class hittableList: public hittable {
     public:
+        std::vector<shared_ptr<hittable>> objects;
+
         hittableList() {}
-        hittableList(hittable **l, int n) {list = l; listSize = n;}
+        hittableList(shared_ptr<hittable> object) {add(objects)}
         
-        virtual bool hit(const ray& r, float tMin, float tMax, hitRecord& rec) const;
-        
-        hittable **list;
-        int listSize;
-};
+        void clear() {objects.clear();}
 
-bool hittableList::hit(const ray& r, float tMin, float tMax, hitRecord& rec) const {
-    hitRecord tempRec;
-    bool hitAnything = false;
-    double closestSoFar = tMax;
-
-    for (int i = 0; i < listSize; i++)
-        if(list[i] -> hit(r, tMin, closestSoFar, tempRec)){
-            hitAnything = true;
-            closestSoFar = tempRec.t;
-            rec = tempRec;
+        void add(shared_ptr<hittable> object){
+            objects.push_back(object);
         }
+
+        virtual bool hit(const ray& r, float tMin, float tMax, hitRecord& rec) const override {
+            hitRecord tempRec;
+            bool hitAnything = false;
+            double closestSoFar = rayT.max;
+
+            for (const auto& object : objects)
+                if(object -> hit(r, interval(rayT.min, closestSoFar), tempRec)){
+                    hitAnything = true;
+                    closestSoFar = tempRec.t;
+                    rec = tempRec;
+                }
     
-    return hitAnything;
-}
+            return hitAnything;
+        }
+        
+};
 
 #endif
